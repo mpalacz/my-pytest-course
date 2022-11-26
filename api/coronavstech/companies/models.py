81bbf88269184bf7b034aa05b6bacb607a6de25a
mpalacz <mpalacz00@gmail.com>
2022-11-26 21:12:15 +0100

    Creating first Django app

--- api/coronavstech/companies/models.py
+++ api/coronavstech/companies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import URLField
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Company(models.Model):
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hiring Freeze"
+        HIRING = "Hiring"
+
+    name = models.CharField(max_length=30, unique=True)
+    status = models.CharField(
+        max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING
+    )
+    last_update = models.DateTimeField(default=now, editable=True)
+    application_link = URLField(blank=True)
+
+    def __str__(self):
+        return self.name
