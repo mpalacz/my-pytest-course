72b7ce922c5ca4b6dbca6c8a3f3412da06f19fc9
mpalacz <mpalacz00@gmail.com>
2022-12-28 19:40:07 +0100

    Pytest tests that assert Logs

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -46,16 +46,19 @@ class TestPostCompanies(BasicCompanyAPiTestCase):
         )
 
     def test_create_existing_company_should_fail(self) -> None:
-        Company.objects.create(name='apple')
+        Company.objects.create(name="apple")
         response = self.client.post(path=self.companies_url, data={"name": "apple"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(
-            json.loads(response.content), {"name": ["company with this name already exists."]}
+            json.loads(response.content),
+            {"name": ["company with this name already exists."]},
         )
 
     def test_create_company_with_only_name_all_fields_should_be_default(self) -> None:
         test_company_name = "test company name"
-        response = self.client.post(path=self.companies_url, data={"name": test_company_name})
+        response = self.client.post(
+            path=self.companies_url, data={"name": test_company_name}
+        )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
         self.assertEqual(response_content.get("name"), test_company_name)
@@ -64,13 +67,19 @@ class TestPostCompanies(BasicCompanyAPiTestCase):
         self.assertEqual(response_content.get("notes"), "")
 
     def test_create_company_with_layoffs_status_should_succeed(self) -> None:
-        response = self.client.post(path=self.companies_url, data={"name": "test company name", "status": "Layoffs"})
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "Layoffs"},
+        )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
         self.assertEqual(response_content.get("status"), "Layoffs")
 
     def test_create_company_with_wrong_status_should_fail(self) -> None:
-        response = self.client.post(path=self.companies_url, data={"name": "test company name", "status": "WrongStatus"})
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "WrongStatus"},
+        )
         self.assertEqual(response.status_code, 400)
         self.assertIn("WrongStatus", str(response.content))
         self.assertIn("is not a valid choice", str(response.content))
@@ -83,10 +92,35 @@ class TestPostCompanies(BasicCompanyAPiTestCase):
     def test_should_be_skipped(self) -> None:
         self.assertEqual(1, 2)
 
-    def raise_covid19_exception(self) -> None:
-        raise ValueError("CoronaVirus Exception")
 
-    def test_raise_covid19_exception_should_pass(self) -> None:
-        with pytest.raises(ValueError) as e:
-            self.raise_covid19_exception()
-        assert "CoronaVirus Exception" == str(e.value)
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
+
+
+import logging
+
+logger = logging.getLogger("CORONA_LOGS")
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
