33666a65257410aa381fd5aa5f7e93cf02e3fd7e
mpalacz <mpalacz00@gmail.com>
2022-12-31 11:20:01 +0100

    Caching

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,10 +1,14 @@
+from typing import Callable
+
 import pytest
 
+from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 
 
 # @my_parametrize(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
+@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n: int, expected: int) -> None:
-    res = fibonacci_naive(n=n)
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+    res = fib_func(n)
     assert res == expected
