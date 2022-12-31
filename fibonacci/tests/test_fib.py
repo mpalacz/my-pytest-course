747eed1422516078d7ca5792cd48c1e35861e891
mpalacz <mpalacz00@gmail.com>
2022-12-31 12:40:32 +0100

    parallel pipelines

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -7,8 +7,10 @@ from fibonacci.naive import fibonacci_naive
 
 
 # @my_parametrize(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
-@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+@pytest.mark.parametrize(
+    "fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
+)
 def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
     res = fib_func(n)
     assert res == expected
