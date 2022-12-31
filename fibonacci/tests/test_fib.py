f67bffef42d94898204fad4d196e35b0169c11d6
mpalacz <mpalacz00@gmail.com>
2022-12-31 17:05:40 +0100

    Performance testing

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -5,7 +5,7 @@ import pytest
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.dynamic import fibonacci_dynamic, fibonacci_dynamic_v2
 from fibonacci.naive import fibonacci_naive
-from fixtures import time_tracker
+from conftest import time_tracker
 
 
 # @my_parametrize(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
