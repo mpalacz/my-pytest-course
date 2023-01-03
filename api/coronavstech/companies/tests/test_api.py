38024084cc7ca3a2a8d7cd3718a45f1099d43ed4
mpalacz <mpalacz00@gmail.com>
2023-01-03 12:51:02 +0100

    Executing parametrized fixtures

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 import pytest
 from django.urls import reverse
@@ -124,7 +125,16 @@ def test_logged_info_level(caplog) -> None:
 # Fixtures
 
 
-@pytest.fixture()
+@pytest.fixture
+def companies(request, company) -> List[Company]:
+    companies = []
+    names = request.param
+    for name in names:
+        companies.append(company(name=name))
+    return companies
+
+
+@pytest.fixture
 def company(**kwargs):
     def _company_factory(**kwargs) -> Company:
         company_name = kwargs.pop("name", "TestCompany INC")
@@ -133,11 +143,14 @@ def company(**kwargs):
     return _company_factory
 
 
-def test_multiple_companies_exists_should_succeed(client, company) -> None:
-    tiktok: Company = company(name="TikTok")
-    twitch: Company = company(name="Twitch")
-    test_company: Company = company()
-    company_names = {tiktok.name, twitch.name, test_company.name}
+@pytest.mark.parametrize(
+    "companies",
+    [["TikTok", "Twitch", "Test Company INC"], ["Facebook", "Instagram"]],
+    ids=["3 T companies", "Zuckerberg's companies"],
+    indirect=True,
+)
+def test_multiple_companies_exists_should_succeed(client, companies) -> None:
+    company_names = set(map(lambda x: x.name, companies))
     response_companies = client.get(companies_url).json()
     assert len(company_names) == len(response_companies)
     response_company_names = set(
